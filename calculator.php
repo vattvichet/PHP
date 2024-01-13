0834669d13dae8973260296e34462acecdfecc12
vichet vatt <vichetvattedu@gmail.com>
2024-01-13 15:20:26 +0700

    Finished calculator

---
+++ calculator.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+
+<body>
+
+    <form action="calculator.php" method="post">
+        Number1: <input type="number" name="number1Input">
+        <br><br>
+        Operation: <input type="text" name="operationInput">
+        <br><br>
+        Number2: <input type="number" name="number2Input">
+        <br><br>
+        <input type="submit">
+        <hr>
+    </form>
+
+    <?php
+
+    $num1 = $_POST['number1Input'];
+    $num2 = $_POST['number2Input'];
+    $op = $_POST['operationInput'];
+
+    if ($op == '+') {
+        echo $num1 + $num2;
+    } else if ($op == '-') {
+        echo $num1 - $num2;
+    } else if ($op == '*') {
+        echo $num1 * $num2;
+    } else if ($op == '/') {
+        echo $num1 / $num2;
+    } else {
+        echo "Please choose a valid Operator!";
+    }
+
+    ?>
+</body>
+
+</html>
