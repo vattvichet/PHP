18b84bfe47f4465548fdc622bb64c7a6d885581e
vichet vatt <vichetvattedu@gmail.com>
2024-01-13 16:12:47 +0700

    getter and setter

--- class.php
+++ class.php
@@ -1,10 +1,13 @@
 <?php
 class Person
 {
-    var $id;
-    var $name;
-    var $age;
-    var $gender;
+    public $id;
+    public $name;
+    public $age;
+    public $gender;
+
+    private $salary;
+
 
     function __construct($id, $n, $a, $g)
     {
@@ -12,14 +15,25 @@ class Person
         $this->name = $n;
         $this->age = $a;
         $this->gender = $g;
+    }
 
-
-        echo "<li> Person: " . $id . "";
+    function setSalary($s)
+    {
+        $this->salary = $s;
+    }
+    function getSalary()
+    {
+        return $this->salary;
+    }
+    function getDetail()
+    {
+        echo "<li> Person: " . $this->id . "";
         echo "<br>";
-        echo "\t\t" . "Name: " . $n . "\t" . "Age: " . $a . "\t" . "Gender: " . $g;
+        echo "\t\t" . "Name: " . $this->name . "\t" . "Age: " . $this->age . "\t" . "Gender: " . $this->gender . "\t" . "Salary: " . $this->getSalary();
         echo "<br><br>";
     }
 }
 
 $person1 = new Person(1, "Vichet", 20, "Male");
-$person2 = new Person(2, "John", 20, "Male");
+$person1->setSalary(123);
+$person1->getDetail();
